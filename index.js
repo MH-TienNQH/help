89ef92f50d68feb8b53561e5cd7f5e6a39f57160
MH-TienNQH <tien.nqh@mor.com.vn>
2024-07-16 10:55:32 +0700

    #4 - create verifyToken middlewares

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import rootRouter from "./src/routes/index.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
@@ -8,7 +10,9 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", rootRouter);
 
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { login, signUp } from "../controllers/authControllers.js";
+import { login, logout, signUp } from "../controllers/authControllers.js";
+import verifyTokenMiddlewares from "../middlewares/verifyTokenMiddlewares.js";
 
 export const authRoutes = Router();
 
 authRoutes.post("/signup", signUp);
-authRoutes.post("/login", login);
+authRoutes.post("/login", login, verifyTokenMiddlewares);
+authRoutes.post("/logout", logout);
