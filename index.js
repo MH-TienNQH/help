f82f52adbe087791ff77944bb9eff3edb12e8c43
MH-TienNQH <tien.nqh@mor.com.vn>
2024-07-17 15:12:07 +0700

    turn back cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import rootRouter from "./src/routes/index.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { errorHandlerMiddlewares } from "./src/middlewares/errorHandlerMiddlewares.js";
 
 dotenv.config();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(errorHandlerMiddlewares);
