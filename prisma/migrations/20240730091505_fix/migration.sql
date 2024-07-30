38a3ca925ccfb74747bebefe158b31f23928f66d
MH-TienNQH <tien.nqh@mor.com.vn>
2024-07-30 16:15:25 +0700

    fix

---
+++ prisma/migrations/20240730091505_fix/migration.sql
@@ -0,0 +1,16 @@
+-- AlterTable
+ALTER TABLE `Product` ADD COLUMN `soldAt` DATETIME(3) NULL;
+
+-- CreateTable
+CREATE TABLE `ProductLiked` (
+    `productId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `ProductLiked_productId_userId_key`(`productId`, `userId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `ProductLiked` ADD CONSTRAINT `ProductLiked_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`productId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ProductLiked` ADD CONSTRAINT `ProductLiked_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`userId`) ON DELETE RESTRICT ON UPDATE CASCADE;
