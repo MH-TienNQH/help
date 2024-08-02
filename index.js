6f9228f102060338c6d2953783f6d590b6ba98d1
MH-TienNQH <tien.nqh@mor.com.vn>
2024-08-02 13:38:41 +0700

    ok

--- index.js
+++ index.js
@@ -7,34 +7,19 @@ import schedule from "node-schedule";
 import express from "express";
 import rootRouter from "./src/routes/index.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import { errorHandlerMiddlewares } from "./src/middlewares/errorHandlerMiddlewares.js";
 import { deleteNotVerified } from "./src/utils/deleteNotVerified.js";
-import { expireOTP } from "./src/utils/expireOTP.js";
 
 const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", rootRouter);
 app.use(errorHandlerMiddlewares);
 
-app.get("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
-});
-
 schedule.scheduleJob("*/10 * * * *", deleteNotVerified);
-schedule.scheduleJob("*/1 * * * *", expireOTP);
 
 app.listen(PORT, () => `running on http://localhost:${PORT}`);
