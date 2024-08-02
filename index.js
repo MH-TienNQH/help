93d0ebe3d5dd5787fa890dae24739f3498695dec
MH-TienNQH <tien.nqh@mor.com.vn>
2024-08-02 16:29:00 +0700

    feature: add cors

--- index.js
+++ index.js
@@ -19,19 +19,15 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(paginationMiddleware);
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+// app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api", rootRouter);
 app.use(errorHandlerMiddlewares);
-app.get("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
-});
 
 app.listen(PORT, () => `running on http://localhost:${PORT}`);
