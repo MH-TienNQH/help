42b4c547b37dbc47555e3805fc430c804588e6d5
MH-TienNQH <tien.nqh@mor.com.vn>
2024-08-02 16:06:09 +0700

    feature:add cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import cors from "cors";
+import { crossOriginResourcePolicy } from "helmet";
 // //cron
 // import schedule from "node-schedule";
 
@@ -19,9 +20,20 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(paginationMiddleware);
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 app.use("/api", rootRouter);
 app.use(errorHandlerMiddlewares);
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+});
 
 app.listen(PORT, () => `running on http://localhost:${PORT}`);
