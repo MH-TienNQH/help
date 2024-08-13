58959732346ed66da21740cdc457a76ed8c1fc3b
MH-TienNQH <tien.nqh@mor.com.vn>
2024-08-13 15:16:28 +0700

    update: add swagger

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,9 +1,6 @@
 import { hashSync } from "bcrypt";
 import { prismaClient } from "../routes/index.js";
 import { OperationalException } from "../exceptions/operationalExceptions.js";
-import { responseFormat } from "../utils/responseFormat.js";
-import { sendMailTo } from "../utils/sendMail.js";
-
 export const getAllUser = async () => {
   const users = await prismaClient.user.findMany();
   const usersWithImageUrls = users.map((user) => ({
