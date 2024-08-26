95e26aa46958a1c34dc4e30e735049ef632b6526
MH-TienNQH <tien.nqh@mor.com.vn>
2024-08-26 09:52:15 +0700

    fix:fix price schema

--- src/schema/productSchema.js
+++ src/schema/productSchema.js
@@ -3,12 +3,19 @@ import { z } from "zod";
 export const addProductSchema = z.object({
   name: z.string().min(1, "Product name can not be empty"),
   description: z.string().min(1, "Product description can not be empty"),
+  price: z
+    .number()
+    .positive("Price must be greater than 0")
+    .int("Price must be a whole number"),
   images: z.string().optional(),
 });
 
 export const updateProductSchema = z.object({
   name: z.string().min(1, "Product name can not be empty"),
   description: z.string().min(1, "Product description can not be empty"),
-
+  price: z
+    .number()
+    .positive("Price must be greater than 0")
+    .int("Price must be a whole number"),
   images: z.string().optional(),
 });
